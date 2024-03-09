aa088e1c6bfb853ea8ed0ea409e638a39c113c58
a <>
2024-03-08 23:45:52 -0600

    init

---
+++ app/@modal/(.)photos/[id]/page.tsx
@@ -0,0 +1,7 @@
+export default function PhotoModal({
+    params: { id: photoId },
+}: {
+    params: { id: string }
+}) {
+    return <div className="absolute inset-0 h-full w-full bg-black">reij</div>
+}
